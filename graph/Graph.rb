fdab4d7ed094329d7a18f4dca1c929a5887a0373
Varsion <JianhuaL12@outlook.com>
2021-02-19 18:14:52 +0800

    some else about node,linkedlist and graph

--- graph/Graph.rb
+++ graph/Graph.rb
@@ -1,3 +1,14 @@
 class Graph
+	attr_accessor :container, :count
 
+	def initialize(count)
+	  @count = count
+	end
+
+
+	# Breadth First Search
+	# 广度优先搜索
+	def bfs start, end
+		
+	end
 end
---
+++ list/node.rb
@@ -0,0 +1,8 @@
+class Node
+	attr_accessor :item, :next
+
+	def initialize(item)
+	  @item = item
+	  @next = nil
+	end
+end
